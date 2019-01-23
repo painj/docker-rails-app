be64c7c823714aea00c7400b0f4e0c3484e935ea
painjsimpson <painjsimpson@gmail.com>
2019-01-23 04:55:41 -0700

    Bootstrap and users

---
+++ db/migrate/20190123062113_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.string :password_digest
+      t.boolean :activated
+      t.boolean :enabled
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_090215) do
+ActiveRecord::Schema.define(version: 2019_01_23_062113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 2019_01_21_090215) do
   create_table "pages", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "activated"
+    t.boolean "enabled"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
